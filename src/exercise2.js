1cd91feac23c3c44cca82bcd8d55d0e31aa6013c
Nicolas <nicolas.durand@ynov.com>
2017-11-21 16:26:01 +0100

    Exercice 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,14 @@
 exports.run = function(input) {
+  var array = input.split("")
+  var biggest = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > biggest) {
+      biggest = array[i]
+    } else {
+      biggest = biggest
+    }
+  }
+  biggest = parseInt(biggest, 10)
+  return biggest
   console.log("Exercise #2");
 };

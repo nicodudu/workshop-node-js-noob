f1d65f6e90ad5b80be473a0e21d54ba4be9b6046
Nicolas <nicolas.durand@ynov.com>
2017-11-21 17:05:59 +0100

    Exercice 5 pas fini

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,11 @@
 exports.run = function(input) {
+  var fs = require('fs');
+  fs.readdir(input,function(err, files) {
+    number = files.length()
+  })
+  showInProgress: () => console.log("In progress ...");
+  showError: () => console.log("Something goes wrong");
+  showDone: number => console.log(`Project contains ${number} file(s) of tests`);
+  
   console.log("Exercise #5");
 };
